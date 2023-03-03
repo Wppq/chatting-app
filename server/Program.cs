5cf781e5203f53418b3684d44174cd3b11837871
kautsar_masuara <kautsarmasuara@gmail.com>
2023-03-04 05:55:23 +0800

    setup basic server socket

---
+++ server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Chat_App 
+{
+    public class Program 
+    {
+        private static List<Socket> clients = new List<Socket>();
+        private static TcpListener tcpListener = new TcpListener(IPAddress.Parse("localhost"),8080);
+        public static void Main()
+        {
+            tcpListener.Start();
+            while(true)
+            {
+                Socket client = tcpListener.AcceptSocket();
+                if(client.Connected)
+                {
+                    clients.Add(client);
+                    Thread newThread = new Thread(() => Listeners(client));
+                    newThread.Start();
+                }
+            }
+        }
+
+        public static void Listeners(Socket client)
+        {
+            Console.WriteLine("Client : " + client.RemoteEndPoint + "now connected");
+        }
+    }
+}
