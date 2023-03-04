8c0453ec4875bc258a75efe7cafada7f3ed0a850
kautsar_masuara <kautsarmasuara@gmail.com>
2023-03-04 17:02:16 +0800

    change folder structure

---
+++ client/Client.cs
@@ -0,0 +1,37 @@
+using System.Net.Socket;
+using System.Text;
+
+namespace ClientConsole
+{
+    public class Client
+    {
+        public Socket Socket { get; private set; }
+        public void StartChatting()
+        {
+            Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket.Connect("192.168.1.16", 3000);
+            Thread Thread = new Thread(GetMessages);
+            Thread.Start();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                byte[] message = Encoding.Unicode.GetBytes(input);
+                Socket.Send(message);
+            }
+        }
+        private void GetMessages()
+        {
+            while (true)
+            {
+                byte[] data = new byte[512];
+                int res = Socket.Receive(data);
+                if (res > 0)
+                {
+                    string message = Encoding.Unicode.GetString(data);
+                    Console.WriteLine(message.Trim('\0'));
+                }
+
+            }
+        }
+    }
+}
